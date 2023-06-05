f7a39537e10b4ba8e4d0b0b19fe03b6b1464d36e
Josue Lopez (Jela) <jela3105@gmail.com>
2023-06-04 19:27:35 -0600

    Created template

--- main.js
+++ main.js
@@ -1,7 +1,22 @@
-    ///import info from info.json;
-function showInfo(symbol, title, description) {
-    document.getElementById('info-title').textContent = symbol + '. ' + title;
-    document.getElementById('info-description').textContent = description;
-    document.getElementById('info-box').style.display = 'block';
+import info from "./info.json" assert { type: "json" };
+
+document.getElementById("info-img").style.height = "300px";
+
+for (let e of info) {
+  let table = document.getElementById("p-table");
+  let element = document.createElement("div");
+  element.textContent = e.titulo;
+  element.className = "element";
+
+  element.addEventListener("click", () => {
+    document.getElementById("info-title").textContent = e.titulo;
+    document.getElementById("info-description").textContent = e.descripcion;
+    document.getElementById("info-box").style.display = "block";
+    document.getElementById("info-img").src = e.img;
+  });
+  table.appendChild(element);
 }
- 
+
+document.getElementById("cerrar").addEventListener("click", () => {
+  document.getElementById("info-box").style.display = "none";
+});
