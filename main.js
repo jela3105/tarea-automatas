984bcc27f7734c6b16a047d35b8e7e777e62db11
Josue Lopez (Jela) <jela3105@gmail.com>
2023-06-05 21:27:00 -0600

    Add index

--- main.js
+++ main.js
@@ -4,10 +4,18 @@ document.getElementById("info-img").style.height = "300px";
 let contador = 1;
 for (let e of info) {
   let table = document.getElementById("p-table");
+
+  let general = document.createElement("div");
+  general.className = "element";
+
+  let title = document.createElement("div");
+  title.style.fontSize = "0.6rem"
+  title.style.textAlign= "left"
+  title.textContent = contador + "";
+
   let element = document.createElement("div");
 
   element.textContent =e.titulo.charAt(0).toUpperCase() + e.titulo.slice(1).toLowerCase(); 
-  element.className = "element";
 
   element.addEventListener("click", () => {
     document.getElementById("info-title").textContent = e.titulo.charAt(0).toUpperCase() + e.titulo.slice(1).toLowerCase();
@@ -16,7 +24,10 @@ for (let e of info) {
     document.getElementById("info-box").style.display = "block";
     document.getElementById("info-img").src = e.img;
   });
-  table.appendChild(element);
+
+  general.appendChild(title);
+  general.appendChild(element);
+  table.appendChild(general);
   contador += 1;
 }
 
